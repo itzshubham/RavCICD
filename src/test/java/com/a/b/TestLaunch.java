49ba2fd6df16a6d23ee2af31df05b4c3423203f1
Shubham Mathur <Shubham.mathur1@builder.ai>
2025-07-14 21:03:45 +0530

    initial commit

---
+++ src/test/java/com/a/b/TestLaunch.java
@@ -0,0 +1,18 @@
+package com.a.b;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestLaunch {
+	
+	@Test
+	public void testLaunch() throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demo.automationtesting.in/");
+		Thread.sleep(3000);
+		driver.quit();
+		
+	}
+
+}
